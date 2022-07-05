4660a261a189290db4dc031d269371522c27669b
sachinkiranti <sachinkiranti@gmail.com>
2022-07-06 01:37:48 +0545

    chore: add config keys

--- config/addy.php
+++ config/addy.php
@@ -2,4 +2,16 @@
 
 return [
 
+    'ignore_route_names' => [
+        'debugbar.openhandler',
+        'debugbar.clockwork',
+        'debugbar.telescope',
+        'debugbar.assets.css',
+        'debugbar.assets.js',
+        'debugbar.cache.delete',
+    ],
+
+    'filters' => [
+        \SachinKiranti\Addy\Filters\IgnoreRoutes::class,
+    ],
 ];
